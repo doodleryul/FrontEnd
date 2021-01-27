f5391b314e351d1163d63aec780fab6dc2fe935d
율링 <76838077+rimiiii@users.noreply.github.com>
2021-01-27 21:00:27 +0900

    Create script.js file

---
+++ youtube_clone/script.js
@@ -0,0 +1,7 @@
+const moreBtn = document.querySelector('.infoAndUpnext .info .metadata .titleAndButton .moreBtn');
+const title = document.querySelector('.infoAndUpnext .info .metadata .titleAndButton .title');
+
+moreBtn.addEventListener('click', () => {
+  moreBtn.classList.toggle('clicked');
+  title.classList.toggle('clamp');
+});
