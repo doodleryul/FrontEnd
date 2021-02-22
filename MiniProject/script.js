ac4e6a0ce0d42940ca18a6a4ee3ed668d61ecade
율링 <76838077+rimiiii@users.noreply.github.com>
2021-02-23 00:23:29 +0900

    Create json file in script.js

---
+++ MiniProject/script.js
@@ -0,0 +1,33 @@
+const clothesInfo = [
+{name: "21S/S 메종키츠네 레귤러 폭스헤드", 
+color: "black", 
+category: "neat", 
+url: "https://m.wdrobe.com/web/product/big/202101/a13adcb15faa36de6c118ce050d6ad61.jpg"},
+{name: "21S/S 메종키츠네 폭스 헤드 패치", 
+color: "grey", 
+category: "sweatshirts", 
+url: "https://m.wdrobe.com/web/product/big/202101/78e7fd008b72192d7b345347b3551f71.jpg"},
+{name: "21S/S 메종키츠네 더블 폭스 헤드 패치", 
+color: "ivory", 
+category: "t-shirt",
+url: "https://m.wdrobe.com/web/product/big/202008/d813f6fbff9a0f1a4e12102046c52161.jpg"},
+{name: "21S/S 메종키츠네 더블 폭스 헤드 패치", 
+color: "black", 
+category: "t-shirt",
+url: "https://m.wdrobe.com/web/product/big/202101/3efc4169b5df5f5325a56c657de7c499.jpg"},
+{name: "21S/S 메종키츠네 클래식크루 폭스패치", 
+color: "ivory", 
+category: "sweatshirts",
+url: "https://m.wdrobe.com/web/product/big/202101/ed3130d3ee123a6038adea19eb2a74c2.jpg"},
+{name: "20F/W 메종키츠네 폭스헤드 패치", 
+color: "grey", 
+category: "sweatshirts",
+url: "https://m.wdrobe.com/web/product/big/202011/01ef37b3efc677ef310858660a00edfd.jpg"},
+{name: "20F/W 메종키츠네 폭스헤드 패치", 
+color: "black", 
+category: "neat",
+url: "https://m.wdrobe.com/web/product/big/202011/6d8fd500651f0b00b40d5c119ead2394.jpg"}
+];
+
+const json = JSON.stringify(clothesInfo, null, 2);
+console.log(json);
